0580aa8b05a13aa44e6810894f98848121d975fa
Antu <122485403+antukung@users.noreply.github.com>
2023-12-01 13:13:10 -0300

    2 commit

--- backent.py
+++ backent.py
@@ -16,16 +16,16 @@ ma=Marshmallow(app)   #crea el objeto ma de de la clase Marshmallow
 
 
 # defino la tabla
-class Producto(db.Model):   # la clase Producto hereda de db.Model    
+class Libro(db.Model):   # la clase Producto hereda de db.Model    
     id=db.Column(db.Integer, primary_key=True)   #define los campos de la tabla
     nombre=db.Column(db.String(100))
-    precio=db.Column(db.Integer)
-    stock=db.Column(db.Integer)
+    link_descarga=db.Column(db.String(400))
+    grupo=db.Column(db.String(400))
     imagen=db.Column(db.String(400))
-    def __init__(self,nombre,precio,stock,imagen):   #crea el  constructor de la clase
+    def __init__(self,nombre,link_descarga,grupo,imagen):   #crea el  constructor de la clase
         self.nombre=nombre   # no hace falta el id porque lo crea sola mysql por ser auto_incremento
-        self.precio=precio
-        self.stock=stock
+        self.link_descarga=link_descarga
+        self.grupo=grupo
         self.imagen=imagen
 
 
@@ -40,15 +40,15 @@ class Producto(db.Model):   # la clase Producto hereda de db.Model
 with app.app_context():
     db.create_all()  # aqui crea todas las tablas
 #  ************************************************************
-class ProductoSchema(ma.Schema):
+class LibroSchema(ma.Schema):
     class Meta:
-        fields=('id','nombre','precio','stock','imagen')
+        fields=('id','nombre','link_descarga','grupo','imagen')
 
 
 
 
-producto_schema=ProductoSchema()            # El objeto producto_schema es para traer un producto
-productos_schema=ProductoSchema(many=True)  # El objeto productos_schema es para traer multiples registros de producto
+libro_schema=LibroSchema()            # El objeto libro_schema es para traer un producto
+libros_schema=LibroSchema(many=True)  # El objeto libros_schema es para traer multiples registros de producto
 
 
 
@@ -56,8 +56,8 @@ productos_schema=ProductoSchema(many=True)  # El objeto productos_schema es para
 # crea los endpoint o rutas (json)
 @app.route('/productos',methods=['GET'])
 def get_Productos():
-    all_productos=Producto.query.all()         # el metodo query.all() lo hereda de db.Model
-    result=productos_schema.dump(all_productos)  # el metodo dump() lo hereda de ma.schema y
+    all_productos=Libro.query.all()         # el metodo query.all() lo hereda de db.Model
+    result=libros_schema.dump(all_productos)  # el metodo dump() lo hereda de ma.schema y
                                                  # trae todos los registros de la tabla
     return jsonify(result)                       # retorna un JSON de todos los registros de la tabla
 
@@ -66,51 +66,51 @@ def get_Productos():
 
 @app.route('/productos/<id>',methods=['GET'])
 def get_producto(id):
-    producto=Producto.query.get(id)
-    return producto_schema.jsonify(producto)   # retorna el JSON de un producto recibido como parametro
+    producto=Libro.query.get(id)
+    return libro_schema.jsonify(producto)   # retorna el JSON de un producto recibido como parametro
 
 
 
 
 @app.route('/productos/<id>',methods=['DELETE'])
 def delete_producto(id):
-    producto=Producto.query.get(id)
+    producto=Libro.query.get(id)
     db.session.delete(producto)
     db.session.commit()
-    return producto_schema.jsonify(producto)   # me devuelve un json con el registro eliminado
+    return libro_schema.jsonify(producto)   # me devuelve un json con el registro eliminado
 
 
 @app.route('/productos', methods=['POST']) # crea ruta o endpoint
 def create_producto():
     #print(request.json)  # request.json contiene el json que envio el cliente
     nombre=request.json['nombre']
-    precio=request.json['precio']
-    stock=request.json['stock']
+    link_descarga=request.json['link_descarga']
+    grupo=request.json['grupo']
     imagen=request.json['imagen']
-    new_producto=Producto(nombre,precio,stock,imagen)
+    new_producto=Libro(nombre,link_descarga,grupo,imagen)
     db.session.add(new_producto)
     db.session.commit()
-    return producto_schema.jsonify(new_producto)
+    return libro_schema.jsonify(new_producto)
 
 
 @app.route('/productos/<id>' ,methods=['PUT'])
 def update_producto(id):
-    producto=Producto.query.get(id)
+    producto=Libro.query.get(id)
  
     nombre=request.json['nombre']
-    precio=request.json['precio']
-    stock=request.json['stock']
+    link_descarga=request.json['link_descarga']
+    grupo=request.json['grupo']
     imagen=request.json['imagen']
 
 
     producto.nombre=nombre
-    producto.precio=precio
-    producto.stock=stock
+    producto.link_descarga=link_descarga
+    producto.grupo=grupo
     producto.imagen=imagen
 
 
     db.session.commit()
-    return producto_schema.jsonify(producto)
+    return libro_schema.jsonify(producto)
  
 
 
@@ -134,16 +134,16 @@ ma=Marshmallow(app)   #crea el objeto ma de de la clase Marshmallow
 
 
 # defino la tabla
-class Producto(db.Model):   # la clase Producto hereda de db.Model    
+class Libro(db.Model):   # la clase Producto hereda de db.Model    
     id=db.Column(db.Integer, primary_key=True)   #define los campos de la tabla
     nombre=db.Column(db.String(100))
-    precio=db.Column(db.Integer)
-    stock=db.Column(db.Integer)
+    link_descarga=db.Column(db.String(400))
+    grupo=db.Column(db.String(400))
     imagen=db.Column(db.String(400))
-    def __init__(self,nombre,precio,stock,imagen):   #crea el  constructor de la clase
+    def __init__(self,nombre,link_descarga,grupo,imagen):   #crea el  constructor de la clase
         self.nombre=nombre   # no hace falta el id porque lo crea sola mysql por ser auto_incremento
-        self.precio=precio
-        self.stock=stock
+        self.link_descarga=link_descarga
+        self.grupo=grupo
         self.imagen=imagen
 
 
@@ -158,15 +158,15 @@ class Producto(db.Model):   # la clase Producto hereda de db.Model
 with app.app_context():
     db.create_all()  # aqui crea todas las tablas
 #  ************************************************************
-class ProductoSchema(ma.Schema):
+class LibroSchema(ma.Schema):
     class Meta:
-        fields=('id','nombre','precio','stock','imagen')
+        fields=('id','nombre','link_descarga','grupo','imagen')
 
 
 
 
-producto_schema=ProductoSchema()            # El objeto producto_schema es para traer un producto
-productos_schema=ProductoSchema(many=True)  # El objeto productos_schema es para traer multiples registros de producto
+libro_schema=LibroSchema()            # El objeto libro_schema es para traer un producto
+libros_schema=LibroSchema(many=True)  # El objeto libros_schema es para traer multiples registros de producto
 
 
 
@@ -174,8 +174,8 @@ productos_schema=ProductoSchema(many=True)  # El objeto productos_schema es para
 # crea los endpoint o rutas (json)
 @app.route('/productos',methods=['GET'])
 def get_Productos():
-    all_productos=Producto.query.all()         # el metodo query.all() lo hereda de db.Model
-    result=productos_schema.dump(all_productos)  # el metodo dump() lo hereda de ma.schema y
+    all_productos=Libro.query.all()         # el metodo query.all() lo hereda de db.Model
+    result=libros_schema.dump(all_productos)  # el metodo dump() lo hereda de ma.schema y
                                                  # trae todos los registros de la tabla
     return jsonify(result)                       # retorna un JSON de todos los registros de la tabla
 
@@ -184,51 +184,51 @@ def get_Productos():
 
 @app.route('/productos/<id>',methods=['GET'])
 def get_producto(id):
-    producto=Producto.query.get(id)
-    return producto_schema.jsonify(producto)   # retorna el JSON de un producto recibido como parametro
+    producto=Libro.query.get(id)
+    return libro_schema.jsonify(producto)   # retorna el JSON de un producto recibido como parametro
 
 
 
 
 @app.route('/productos/<id>',methods=['DELETE'])
 def delete_producto(id):
-    producto=Producto.query.get(id)
+    producto=Libro.query.get(id)
     db.session.delete(producto)
     db.session.commit()
-    return producto_schema.jsonify(producto)   # me devuelve un json con el registro eliminado
+    return libro_schema.jsonify(producto)   # me devuelve un json con el registro eliminado
 
 
 @app.route('/productos', methods=['POST']) # crea ruta o endpoint
 def create_producto():
     #print(request.json)  # request.json contiene el json que envio el cliente
     nombre=request.json['nombre']
-    precio=request.json['precio']
-    stock=request.json['stock']
+    link_descarga=request.json['link_descarga']
+    grupo=request.json['grupo']
     imagen=request.json['imagen']
-    new_producto=Producto(nombre,precio,stock,imagen)
+    new_producto=Libro(nombre,link_descarga,grupo,imagen)
     db.session.add(new_producto)
     db.session.commit()
-    return producto_schema.jsonify(new_producto)
+    return libro_schema.jsonify(new_producto)
 
 
 @app.route('/productos/<id>' ,methods=['PUT'])
 def update_producto(id):
-    producto=Producto.query.get(id)
+    producto=Libro.query.get(id)
  
     nombre=request.json['nombre']
-    precio=request.json['precio']
-    stock=request.json['stock']
+    link_descarga=request.json['link_descarga']
+    grupo=request.json['grupo']
     imagen=request.json['imagen']
 
 
     producto.nombre=nombre
-    producto.precio=precio
-    producto.stock=stock
+    producto.link_descarga=link_descarga
+    producto.grupo=grupo
     producto.imagen=imagen
 
 
     db.session.commit()
-    return producto_schema.jsonify(producto)
+    return libro_schema.jsonify(producto)
  
 
 
